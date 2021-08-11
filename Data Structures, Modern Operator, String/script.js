f5decebe0adaab33a69fbcc48270581a40e733b3
Pawasthi0512 <pawasthi0512@gmail.com>
2021-08-12 00:51:27 +0530

    We have completed destructuring of array and objects

--- Data Structures, Modern Operator, String/script.js
+++ Data Structures, Modern Operator, String/script.js
@@ -1,13 +1,13 @@
 'use strict';
 
 // Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  deliveryLocation: ['Via Angelo Tavanti 23', 'Firenze', 'Italy'],
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
@@ -26,4 +26,50 @@ const restaurant = {
       close: 24,
     },
   },
+  /*
+  //openDelivery: function(obj){
+  //   console.log(obj);
+  // } these both are same
+  openDelivery: function(a,c,b){
+    console.log(a,b,c);
+  }
+  */
 };
+
+// destructuring  objects
+// const {name,deliveryLocation, openingHours}=restaurant;
+// console.log(name,deliveryLocation, openingHours);
+
+// Giving variable name to object name
+// const {name: restaurantName,deliveryLocation: city, openingHours:openingTime}=restaurant;
+// console.log(restaurantName,city,openingTime);
+
+/*
+////// Assigning default value to a destructuring variable///////
+const {name: restaurantName=7,deliveryLocation: city, open= 8}=restaurant;
+console.log(restaurantName,city, open);//If 'open' will be present in our 'restaurant' object then its
+value will be assigned to 'open' variable otherwise it will carry default value.
+*/
+
+/*
+/////////Nested objects///////////
+const {
+  openingHours:{thu:{open: openThus,closeThus},
+  fri:{ open:openFri,close:closeFri},},
+}=restaurant;
+console.log(openThus,closeFri);
+*/
+
+/*
+//////// Reassigning a variable through destructuring//////
+let a=11,b=12;
+const obj={a:23,b:34};
+({a,b}=obj);/////We have to put "Parenthesis"
+console.log(a,b)
+*/
+
+/*
+///// Passing an object to a object Method/////////
+const obj={ a:4,b:5,c:6};
+restaurant.openDelivery(obj);
+*/
