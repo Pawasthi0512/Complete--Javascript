73bf9751c24e198118a722f5e84d2a640b6090c8
Pawasthi0512 <pawasthi0512@gmail.com>
2021-08-14 22:15:22 +0530

    We have completed codding challange 2

--- Data Structures, Modern Operator, String/script.js
+++ Data Structures, Modern Operator, String/script.js
@@ -276,3 +276,109 @@ for(const [index,item] of arr.entries()){
   console.log(`${index+1}:- ${item}`);
 }
 */
+
+
+/*
+//.........Looping of Objects...........
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+  ['Neuer','Pavard','Martinez','Alaba','Davies','Kimmich','Goretzka','Coman','Muller','Gnarby','Lewandowski'],
+  ['Burki','Schulz','Hummels','Akanji','Hakimi','Weigl','Witsel','Hazard','Brandt','Sancho','Gotze']
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+  'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+  team1: 1.33,
+  x: 3.25,
+  team2: 6.5,
+  },
+};
+
+/////Accessing all Keys of any Object///// 
+// const keyArr=Object.keys(game);
+// const keyArr=Object.keys(game.odds);
+
+/////Accessing all values of any Object/////
+// const valArr= Object.values(game);
+
+/////Accessing all keys and values of any Object/////
+// const properties= Object.entries(game);
+// for(const [key,value] of properties)
+// console.log(`This is Key ${key} and this is Value ==>>${value}`);
+*/
+
+
+/*
+................Coding Challenge #2................
+
+Let's continue with our football betting app! Keep using the 'game' variable from 
+before.
+Your tasks:
+1. Loop over the game.scored array and print each player name to the console, 
+along with the goal number (Example: "Goal 1: Lewandowski")
+2. Use a loop to calculate the average odd and log it to the console (We already 
+studied how to calculate averages, you can go check if you don't remember)
+3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+Odd of victory Bayern Munich: 1.33
+Odd of draw: 3.25
+Odd of victory Borrussia Dortmund: 6.5
+Get the team names directly from the game object, don't hardcode them 
+(except for "draw"). Hint: Note how the odds and the game objects have the 
+same property names �
+4. Bonus: Create an object called 'scorers' which contains the names of the 
+players who scored as properties, and the number of goals as the value. In this 
+game, it will look like this:
+{
+ Gnarby: 1,
+ Hummels: 1,
+ Lewandowski: 2
+}
+*/
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+  ['Neuer','Pavard','Martinez','Alaba','Davies','Kimmich','Goretzka','Coman','Muller','Gnarby','Lewandowski'],
+  ['Burki','Schulz','Hummels','Akanji','Hakimi','Weigl','Witsel','Hazard','Brandt','Sancho','Gotze']
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+  'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+  team1: 1.33,
+  x: 3.25,
+  team2: 6.5,
+  },
+};
+//1.
+// const entry= Object.entries(game);
+// const [key,val]= Object.entries(game)[4];
+// for(const [goal,player] of val.entries()){
+//   console.log(`Goal ${goal+1}: ${player}`);
+// }
+
+//2.
+// const odd=Object.values(game.odds);
+// let avg=0;
+// for(const item of odd){
+//   avg+=item;
+// }
+// avg=avg/3;
+// console.log(avg);
+
+//3.
+// const odd=Object.entries(game.odds);
+// for(let [key,value] of odd){
+
+//   const str=key==='x'?'draw':` ${game[key]}`;
+//   console.log(` Odd of victory ${str} : ${value}`);
+// }
+
+
